d2c37f42c571e383f33db6dba9d78300ca597702
Alex sandro Zastrow Wickeinecki <53551414+Alexszw@users.noreply.github.com>
2019-10-14 23:09:20 -0300

    Add files via upload

--- teste.py
+++ teste.py
@@ -1,12 +1,36 @@
+import random
 print('***************************************************')
 print('********** Bem vindo ao jogo da advinhação ********')
 print('***************************************************')
-chute = int(input('Digite um número :'))
-numero_secreto: int = 23
-for n in range(1, 3):
-	if chute == numero_secreto:
-		print('Você acertou !')
-		break
-	else :
-		print('Você errou !')
-		chute = int(input('Digite novamente :'))
+print('Digite a dificuldade do jogo :')
+print('Digite (1) para fácil (2) para médio (3) para difícil')
+dificuldade = int(input('Qual a dificuldade :'))
+numero_secreto = random.randrange(1,101)
+numero_tentativas = 0
+if dificuldade == 1 :
+	numero_tentativas = 20
+elif dificuldade == 2 :
+	numero_tentativas = 10
+elif dificuldade == 3 :
+	numero_tentativas = 5
+else :
+	print('Valor da dificuldade inválido !')
+
+for n in range(1, numero_tentativas):
+	print(f'Tentativa {n} de {numero_tentativas}')
+	chute = int(input('Digite um número entre 1 e 100 para o chute :'))
+	if chute >= 1 and chute <= 100 :
+		if chute > numero_secreto:
+			print('Você errou ! O número é maior que o número secreto !')
+		elif chute < numero_secreto:
+			print('Você errou) ! O número é menor que o número secreto !')
+		elif chute == numero_secreto:
+			print('Você acertou !')
+			break
+		else :
+			print('O número digitado é inválidp !')
+			break
+
+
+
+
