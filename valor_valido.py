456802272d3deea6aca3c2bb0872cbd023db1ade
Alex sandro Zastrow Wickeinecki <53551414+Alexszw@users.noreply.github.com>
2019-10-18 22:26:31 -0300

    Delete valor_valido.py

--- valor_valido.py
+++
@@ -1,9 +0,0 @@
-#Faça um programa que peça uma nota, entre zero e dez. Mostre uma mensagem caso o valor seja inválido e continue pedindo
-# até que o usuário informe um valor válido.
-nota = float(input('Digite uma nota entre 0 e 10 :'))
-
-while (nota < 0 or nota > 10):
-    print('Valor inválido')
-    nota = float(input('Digite uma nota entre 0 e 10 :'))
-else:
-    print('Valor válido !')
