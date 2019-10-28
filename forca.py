4ca23d36f09dd5b05da43c4de33c7f6137bb68ad
Alex sandro Zastrow Wickeinecki <53551414+Alexszw@users.noreply.github.com>
2019-10-27 22:52:07 -0300

    Add files via upload

---
+++ forca.py
@@ -0,0 +1,12 @@
+def jogar():
+    print('***************************')
+    print('****** Jogo da forca ******')
+    print('***************************')
+    palavra_secreta = "Banana"
+    enforcou = False
+    acertou = False
+    while(not enforcou and not acertou) :
+        chute = input('Digite a letra : ')
+        print('Jogando.....')
+    print('Fim do jogo')
+jogar()
