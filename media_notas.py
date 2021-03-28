c52b13a14b0a0f9ff6055bc81ed12835230a24a5
Alex sandro Zastrow Wickeinecki <53551414+Alexszw@users.noreply.github.com>
2021-03-28 17:55:13 -0300

    Delete media_notas.py

--- media_notas.py
+++
@@ -1,8 +0,0 @@
-#Faça um programa que calcule o mostre a média aritmética de N notas.
-numero_notas = int(input('Qual é o número de notas :'))
-soma_nota = float(0)
-for n in range(1,numero_notas +1) :
-    nota = float(input(f'Digite a {n}° nota :'))
-    soma_nota += nota
-media = soma_nota/numero_notas
-print(f'A média das notas é {media}')
