37d971356a328c5a2fcd502c3f9d717b8684a826
Alex sandro Zastrow Wickeinecki <53551414+Alexszw@users.noreply.github.com>
2021-03-28 18:09:07 -0300

    Add files via upload

---
+++ media.py
@@ -0,0 +1,7 @@
+# Faça um Programa que peça as 4 notas bimestrais e mostre a média.
+
+nota1 = float(input("Digite o valor da primeira nota : "))
+nota2 = float(input("Digite o valor da segunda nota :"))
+nota3 = float(input("Digite o valor da terceira nota :"))
+nota4 = float(input("Digite o valor da quarta nota :"))
+print(f"A média das notas bomestrais é : {(nota1 + nota2 + nota3 + nota4) / 4}")
