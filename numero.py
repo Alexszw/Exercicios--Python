b3bb330bcc97f14de7d241e3d21ab5d5249ab907
Alex sandro Zastrow Wickeinecki <53551414+Alexszw@users.noreply.github.com>
2021-03-28 18:02:10 -0300

    Add files via upload

---
+++ numero.py
@@ -0,0 +1,4 @@
+# Faça um Programa que peça um número e então mostre a mensagem O número informado foi [número].
+
+numero = float(input("Digite um número :"))
+print(f"O número digitado foi : {numero}")
