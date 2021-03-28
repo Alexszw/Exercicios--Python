60de1d0b7db14b3bcd02814b09dd72272a5fc179
Alex sandro Zastrow Wickeinecki <53551414+Alexszw@users.noreply.github.com>
2021-03-28 18:02:33 -0300

    Add files via upload

---
+++ soma.py
@@ -0,0 +1,5 @@
+# Faça um Programa que peça dois números e imprima a soma.
+
+n1 = float(input("Digite  um número : "))
+n2 = float(input("Digite outro número : "))
+print(f"A soma dos dois números é : {n1 + n2}")
