a6d45e81bf380c950913a50437d372c207b0ccf2
Alex sandro Zastrow Wickeinecki <53551414+Alexszw@users.noreply.github.com>
2021-03-29 22:56:25 -0300

    Add files via upload

---
+++ converte.py
@@ -0,0 +1,3 @@
+# Faça um Programa que converta metros para centímetros.
+medida = float(input("Digite o valor em metros : "))
+print(f"O valor de {medida} metros é igual a {medida * 100} centimetros")
