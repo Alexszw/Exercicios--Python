9fe911d1db1ded09cd13d751752a4150d40c97c6
Alex sandro Zastrow Wickeinecki <53551414+Alexszw@users.noreply.github.com>
2021-03-29 22:56:46 -0300

    Add files via upload

---
+++ area_circulo.py
@@ -0,0 +1,3 @@
+# Faça um Programa que peça o raio de um círculo, calcule e mostre sua área.
+raio = float(input("Digite o raio do circulo : "))
+print(f"A área do circulo cujo o raio é {raio} é : {3.14 * (raio ** 2)}")
