cc2fac35aa8a390d5e178abbf2e26ba7fcb04584
Alex sandro Zastrow Wickeinecki <53551414+Alexszw@users.noreply.github.com>
2021-03-29 23:05:31 -0300

    Add files via upload

---
+++ area_quadrado.py
@@ -0,0 +1,4 @@
+# Faça um Programa que calcule a área de um quadrado, em seguida mostre o dobro desta área para o usuário.
+
+lado_quadrado = float(input("Digite o valor do lado do quadrado : "))
+print(f"O dobro da área do quadrado no qual o lado é {lado_quadrado} fica : {(lado_quadrado ** 2) * 2}")
