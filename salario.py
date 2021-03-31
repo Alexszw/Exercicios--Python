217a1dfbf78d9919e650b9d029bb7f1928d455e2
Alex sandro Zastrow Wickeinecki <53551414+Alexszw@users.noreply.github.com>
2021-03-30 22:44:17 -0300

    Add files via upload

---
+++ salario.py
@@ -0,0 +1,6 @@
+# Faça um Programa que pergunte quanto você ganha por hora e o número de horas trabalhadas no mês.
+# Calcule e mostre o total do seu salário no referido mês.
+valor_hora = float(input("Digite o valor da sua hora trabalhada R$ :"))
+horas_trabalhadas = float(input("Digite a quantidade de horas trabalhadas :"))
+print(f"O Salário do referido mês é : R$ {valor_hora * horas_trabalhadas}")
+
